85184d51aaef0ede8cc0a1cbf6bedc5b5f458f63
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-06-23 18:24:02 -0300

    feat: header component

---
+++ apps/mobile/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import AddIcon from '@/assets/icons/add.svg'
+import NotebookIcon from '@/assets/icons/notebook.svg'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+export function Header() {
+	return (
+		<View className="w-full h-[40%] rounded-b-xl bg-primary flex justify-center items-center">
+			<View className="pt-10">
+				<View className="w-[90%] flex flex-row items-center justify-between">
+					<View className="flex flex-row justify-center gap-2">
+						<NotebookIcon />
+						<Text className="text-base text-white">Produtos</Text>
+					</View>
+					<TouchableOpacity>
+						<AddIcon />
+					</TouchableOpacity>
+				</View>
+			</View>
+		</View>
+	)
+}
