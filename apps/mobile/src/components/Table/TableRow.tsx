04024d07173c31222ec32772957f6c3266396461
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-06-24 16:49:33 -0300

    chore: connect app on api

--- apps/mobile/src/components/Table/TableRow.tsx
+++ apps/mobile/src/components/Table/TableRow.tsx
@@ -4,7 +4,7 @@ import { TableMenuOptions } from './TableMenuOptions'
 export function TableRow() {
 	return (
 		<View
-			className="flex flex-row items-center gap-2 mt-2 py-3 px-2 bg-white rounded-xl relative shadow-md"
+			className="flex flex-row items-center gap-2 mt-2 py-3 px-2 bg-white dark:bg-badge-dark rounded-xl relative shadow-md"
 			style={{ elevation: 5 }}
 		>
 			<Image
@@ -14,23 +14,29 @@ export function TableRow() {
 
 			<View className="w-[70%]">
 				<View className="flex flex-row items-center justify-between">
-					<Text className="text-xs font-semibold">Bolo de Chocolate</Text>
-					<Text className="text-[10px]">Data de Cadastro: 20/06/2024</Text>
+					<Text className="text-xs text-icon-light dark:text-icon-dark font-semibold">
+						Bolo de Chocolate
+					</Text>
+					<Text className="text-[10px] text-icon-light dark:text-icon-dark">
+						Data de Cadastro: 20/06/2024
+					</Text>
 				</View>
 
 				<View className="pt-2">
-					<Text className="text-[10px]">
-						<Text className="font-medium">Descrição:</Text> Nosso Bolo de
-						Chocolate Clássico é um deleite irresistível. Com massa fofinha e
-						úmida feita...
+					<Text className="text-[10px] text-icon-light dark:text-icon-dark">
+						<Text className="font-medium text-icon-light dark:text-icon-dark">
+							Descrição:
+						</Text>{' '}
+						Nosso Bolo de Chocolate Clássico é um deleite irresistível. Com
+						massa fofinha e úmida feita...
 					</Text>
 				</View>
 
 				<View className="pt-2 flex flex-row items-center justify-between">
-					<Text className="flex text-[10px]">
+					<Text className="flex text-[10px] text-icon-light dark:text-icon-dark">
 						<Text className="font-medium">Valor:</Text> R$ 24,99
 					</Text>
-					<Text className="flex text-[10px]">
+					<Text className="flex text-[10px] text-icon-light dark:text-icon-dark">
 						<Text className="font-medium">Quantidade:</Text> 20 Und.
 					</Text>
 				</View>
--- apps/mobile/src/screens/Products.tsx
+++ apps/mobile/src/screens/Products.tsx
@@ -1,8 +1,22 @@
 import { TableRow } from '@/components/Table/TableRow'
 import { container } from '@/styles/container'
+import axios from 'axios'
+import { useEffect } from 'react'
 import { ScrollView, View } from 'react-native'
 
 export function Products() {
+	async function getData() {
+		console.log('Called')
+		await axios
+			.get('http://172.23.200.107:3333')
+			.then((res) => console.log(res.data))
+			.catch((error) => console.error(error))
+	}
+
+	useEffect(() => {
+		getData()
+	}, [])
+
 	return (
 		<View className={container}>
 			<ScrollView>
