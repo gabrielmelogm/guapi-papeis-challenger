537c954ce471c36979c68934a4a17eea698acdfb
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-06-24 14:59:53 -0300

    feat: toggle colorScheme

--- apps/mobile/src/screens/ChangeTheme.tsx
+++ apps/mobile/src/screens/ChangeTheme.tsx
@@ -11,12 +11,14 @@ export function ChangeTheme() {
 				className={`flex flex-row items-center justify-between bg-badge-light dark:bg-badge-dark rounded-xl px-2 ${Platform.OS === 'ios' && 'py-2'}`}
 				style={{ elevation: 5 }}
 			>
-				<Text className="text-sm font-bold">Trocar tema</Text>
+				<Text className="text-sm font-bold text-icon-light dark:text-icon-dark">
+					Trocar tema
+				</Text>
 				<Switch
 					trackColor={{ false: '#f5f5f5', true: '#4C4C4D' }}
 					thumbColor={colorScheme === 'dark' ? '#E9EAF0' : '#4C4C4D'}
 					onValueChange={toggleColorScheme}
-					value={colorScheme === 'light'}
+					value={colorScheme === 'dark'}
 					style={{ elevation: 5 }}
 				/>
 			</View>
